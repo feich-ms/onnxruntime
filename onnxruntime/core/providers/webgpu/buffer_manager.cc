a1ad82de42ff97618c8bc1b73e34c48f5f599197
Fei Chen <feich@microsoft.com>
2025-06-03 14:45:54 +0800

    update metrics file name

--- onnxruntime/core/providers/webgpu/buffer_manager.cc
+++ onnxruntime/core/providers/webgpu/buffer_manager.cc
@@ -12,7 +12,7 @@ namespace onnxruntime {
 namespace webgpu {
 
 namespace {
-constexpr const char* METRICS_FILE = "webgpu_memory_metrics_no_optimization_fast_release.csv";
+constexpr const char* METRICS_FILE = "memory_result_no_memory_optimization_with_early_release.csv";
 constexpr const char* METRICS_HEADER = "Timestamp,TotalMemory(MB),PeakMemory(MB),ActiveBuffers,TotalBuffers,TimeoutMs\n";
 
 constexpr size_t NormalizeBufferSize(size_t size) {
